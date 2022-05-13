15c84b32e71c4b42b8e335cbaa08461ac7516ddc
sabinajahovic <sabinajahovic1995@gmail.com>
2022-05-13 11:28:32 +0200

    java

---
+++ Јava script/java1/app.js
@@ -0,0 +1,99 @@
+// // alert("Nesto");
+// // var name= 'Max'
+// // ime=30;
+// // // Let-za vrednost  i const-vrednost ne menja 
+// // let godine= 30;
+// // console.log(ime + ""+ godine);
+// // godine=25;
+// // console.log(ime + ""+ godine);
+
+// console.log(5 === 5); 
+// // ZA UPOREDJIVANJE MORA 3=
+
+// console.log(5 !== 6);
+// // nije jednako 2x = jer uporedjuje vrednost i tip
+
+// // .console.log(6 < 8);
+// // const ime = 'Max';
+// // const godine = 40;
+// // const meseci = godine * 12;
+
+// // const nesto = prompt("UNESI TVOJE IME");
+// // const godine = prompt ("Unesi tvoje godine");
+// // alert(
+// //     "Ziveo si" + godine * 12 + "meseci"
+// // )
+
+
+
+// let age = 20;
+//  console.log(++age);
+// //  age= age -1
+// // age++ tek u sledecoj iteraciji sabere a ovaj ++age odma to odradi
+
+
+
+// // const age  = "nesto";
+// // if(age >= 18){
+// // console.log ("Punoletan si")
+// // }
+// // else if(age < 18){
+// //     console.log("Nisi punoletan")
+// // }
+
+// // else{
+// //     console.log(
+// //         "Izvrsava se kad  ni jedan slucaj nije ispunjen"
+// //     )
+// // }
+
+
+// const age = prompt("Unesi svije godine");
+// if (age === 18)
+// {console.log("Ti si punoletan");
+// }
+// else if (age > 18 ){
+//     console.log("Ti si odrastao");
+// }
+// else if (age > 12)
+// {console.log(" ti si tinejdzer");
+// }
+// else{
+//     console.log("Ti si dete")
+// }
+
+// logicki operatori 
+// const a = 4;
+// const b = 5;
+// const c= 0;
+//  console.log(a && b && c);
+// //  &&  and operator :oba ako su true onda vrati true inace nece
+
+// // ako su svi tacni vraca poslednji ako je jedan false odma vrati false
+// ||-OR OPERATOR ili ili vazno je da je jedan tacan,vraca prvog
+
+// const a = 4;
+// const b = false;
+// console.log(a || b)
+
+
+const a = 4;
+const b = false;
+console.log( !5 );
+// ! NEGACIJA
+
+// DOMACI
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Јava script/java2/java.js
@@ -0,0 +1,55 @@
+// for ima tri uslova odakle krece koji je uslov i dokle ide i+=2 znaci i=i+2
+// while samo prima uslov,
+// do while obrnuta whlie petlja
+// for(let i=100;i> 0;i--)
+// {console.log (i)
+// }
+// i%2===0 znaci da je broj paran
+
+
+
+// let suma=0
+// for(let i=0; i<=15;i++){
+    
+//     if(i% 2 !==0)
+//     {suma=suma + i}
+// }
+// console.log(suma);
+
+
+// let suma=0
+// for(let i=0; i<10; i++){
+//     if(i%2 ===0){
+//         console.log('Brojac',i)
+        
+//         suma+=i
+//     }
+// }
+// console.log('suma' ,suma);
+
+
+
+
+// let n=20;
+// while(n===20){
+//     console.log("UNUTAR WHILE PETLJE")
+//     break;
+//     continue;
+   
+// }
+// n=21;
+
+
+
+
+
+// let n=20;
+// while(n===20){
+//     console.log("Unutar while");
+//     break;
+// }
+// n=21;
+//  FIZZBUZZ CHALLENGE od 0 do 100 ispisujemo ih; ako je broj deljiv sa 3 pise FIZZ ako je broj deljiv sa 5 buzz ako oboje  fizz buzz to je domaci
+
+
+
