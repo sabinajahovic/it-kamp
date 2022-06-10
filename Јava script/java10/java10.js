2190058e479dc7a96cfe9e3a33abaa9566504595
DESKTOP-8B08GRN\HP <sabinajahovic1995@gmail.com>
2022-06-10 15:39:21 +0200

    init

---
+++ Јava script/java10/java10.js
@@ -0,0 +1,28 @@
+// Imamo tipove podataka primitivni i re
+//false i true--- false kad su obe tacne tacno
+//false ili false---false kad je ili  ako je jedan tacna tacno
+//jedno = znaci uzima vrednost
+// var1=[12,56,54,78,78,98]
+// //slice-parce uzima parce niza is mesta u novu varijabli  01 2 kopiera u novu varijablu
+// //splice-spajanje prvi je inderz i ukljucuje taj br i
+// // var2=var1.slice(0,3)
+
+
+
+// //splice izbrise jedan i doda druge vrednosti
+// var2=var1.splice(2,0,"nova vrednost","hello")
+// console.log (var2)
+
+//Zadatak
+
+const niz=[1,2,3,4,5,6,7,8,9,10]
+// const zadatak=(array)=>{
+//     parni=array.filter((el)=>el%2===0)
+//     // return parni
+//     const suma=parni.reduce((prev,cur)=>prev+cur)
+//     return suma
+// }
+// console.log(zadatak(niz))
+// niz.forEach((el.index) => {
+    
+// });
---
+++ Јava script/objekat1/objekat1.js
@@ -0,0 +1,65 @@
+//skup stvari koji opisuje neku stvar
+//osobine,funkcije atributi su objekti q v p kjuvelue pars
+const myCar={
+    id: 1,
+    marka: "Audi",
+    model: "a4",
+    boja: "crvena",
+    pogon: "prednji",
+    km: 240000,
+    servis:{
+        datum:'04.maj',
+        km: 233848,
+        serviser:"Geko",
+
+    },
+    udaran: true,
+    // udaren: true,
+}
+console.log("Moj auto je marke:", myCar);
+console.log(myCar.servis.serviser)
+console.log(myCar['servis']['serviser'])
+//pozivamo neku vrednost iz objekta myCar.marka ili myCar["marka"]
+//kljucevi su model,boja itd i mora jedna rec camelcase markaAuto  ili snakescase-marka_auta
+
+const radnik={
+    firstName: "Bob",
+    lastName:"Euroblok",
+//     age: 30,
+//    adresa: 'Avnoja',
+//    contact: '06424255279',
+//    job:"programer",
+//    city: "Novi Pazar",
+//  radni_sati: 40,
+
+// dnevnica: 20,
+
+
+// fullName: function(){
+
+//     return this.firstName + " " +this.lastName //referenca za objekat  gde ga upotrebljavamo 
+// }
+
+adresa:{
+ulica: "avnoja",
+broj: "bb",
+grad: "Novi Pazar",
+getAddress: function(){
+    console.log(this)
+}
+}
+}
+
+radnik.adresa.getAddress() //vratice samo objekat adresa
+
+
+
+// console.log(radnik.fullName()) 
+//napravili smo funkciju koja pise ime i prezime
+// console.log("Radnik: ", radnik.firstName,
+//  radnik.lastName,
+// "Radi kao:", radnik.job,"Zivi u", 
+// radnik.city, "i ima ",
+//  radnik.age ,"godina",
+
+// )
